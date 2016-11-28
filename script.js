8b3a4fa2d01f1b3034bfe82ab20e4bfd7aa2a694
yash <yashgulati.g1@gmail.com>
2016-11-28 10:18:14 +0530

     

--- script.js
+++ script.js
@@ -116,19 +116,38 @@ function drawLives() {
 }
 
 function draw() {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // drawBricks();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
-    // drawPaddle();
-    // drawScore();
-    // drawLives();
+    drawPaddle();
+    drawScore();
+    drawLives();
     collisionDetection();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy < ballRadius || y + dy > canvas.height-ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            lives--;
+            if(!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width/2;
+                y = canvas.height-30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width-paddleWidth)/2;
+            }
+        }
     }
 
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
@@ -138,9 +157,9 @@ function draw() {
         paddleX -= 7;
     }
 
-     x += dx;
-     y += dy;
-     requestAnimationFrame(draw);
+    x += dx;
+    y += dy;
+    requestAnimationFrame(draw);
 }
 
 draw();
