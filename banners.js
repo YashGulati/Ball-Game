65c5187fa73d23821a908454ba6c531a7dcf0dc6
yash <yashgulati.g1@gmail.com>
2016-11-28 12:41:36 +0530

     

--- banners.js
+++ banners.js
@@ -2,6 +2,6 @@
 function bannerPause(){
   ctx.font = "50px Arial";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText("Game Paused ", 70, 160);
+  ctx.fillText("Game Paused ", myCanvasWidth/2 - 160, myCanvasHeight/2);
   return;
 }
